7cb7dcafefac3c71f14ab062e6cec951a66b363b
kaischaefer2000 <58391412+kaischaefer2000@users.noreply.github.com>
2019-12-20 12:29:05 +0100

    Fehler beim Anfordern des neuen Passworts
    Wenn das Senden der Email mit dem neuen Passwort nicht geklappt hat kommt der User auf diese Seite. Dann kann er es über den Zrück Button neu versuchen. Hat das Senden der Email geklappt kommt der User auf die "Erfolgreich.php".

---
+++ fehler.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="de">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="/favicon.ico">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://studentenportal.fhws.de/css/app.css?id=521f12362c5a59066c60">
+        <title>Fehler</title>
+    </head>
+
+    <body>
+    <main id="main">
+    <div class="container mt-4">
+                                
+    <div class="mx-auto mt-5" style="max-width: 400px;">
+        <!--header-->
+        <header class="mb-4">
+            <img src="https://studentenportal.fhws.de/img/logo.svg" alt="" width="100" style="padding-left: 1px;">
+            <h3 class="mt-1">
+                <a href="https://studentenportal.fhws.de" class="font-weight-normal quiet-link" style="font-size: 80%;">Es ist ein Fehler aufgetreten!</a>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.151 17.943l-4.143-4.102-4.117 4.159-1.833-1.833 4.104-4.157-4.162-4.119 1.833-1.833 4.155 4.102 4.106-4.16 1.849 1.849-4.1 4.141 4.157 4.104-1.849 1.849z"/></svg>
+                </h3>
+        </header>
+
+        
+        <div class="card">
+            <div class="card-body">
+                <p>Beim anfordern deines Passwortes ist ein Fehler aufgetreten. Bitte überprüfe deine E-Mail und versuche es erneut.</p>
+            </div>
+        </div>
+        <a href="passwort_vergessen.php" class="btn btn-light rounded-pill">
+        <i class="fas fa-fw fa-arrow-circle-left"></i> Zurück</a>
+    </div>
+
+    </body>
+</html>
