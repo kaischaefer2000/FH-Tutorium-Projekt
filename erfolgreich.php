2be9b034df5ff5185d6861f85f032f1e0c816a55
kaischaefer2000 <58391412+kaischaefer2000@users.noreply.github.com>
2019-12-20 12:26:28 +0100

    Mail erfolgreich versandt
    Auf diese Seite wird man weitergeleitet, wenn man erfolgreich ein neues Passwort beim "Passwort vergessen" File erhalten hat. Falls das nicht funktioniert hat kommt man auf die "Fehler.php" Seite.

---
+++ erfolgreich.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="de">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="/favicon.ico">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://studentenportal.fhws.de/css/app.css?id=521f12362c5a59066c60">
+        <title>Erfolgreich angefordert</title>
+    </head>
+
+    <body>
+    <main id="main">
+    <div class="container mt-4">
+                                
+    <div class="mx-auto mt-5" style="max-width: 400px;">
+        <!--header-->
+        <header class="mb-4">
+            <img src="https://studentenportal.fhws.de/img/logo.svg" alt="" width="100" style="padding-left: 1px;">
+            <h3 class="mt-1">
+                <a href="https://studentenportal.fhws.de" class="font-weight-normal quiet-link" style="font-size: 80%;">Passwort erfolgreich angefordert!</a>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6.25 8.891l-1.421-1.409-6.105 6.218-3.078-2.937-1.396 1.436 4.5 4.319 7.5-7.627z"/></svg>            
+            </h3>
+        </header>
+
+        
+        <div class="card">
+            <div class="card-body">
+                <p>Wir haben dir eine E-Mail mit deinem neuen Passwort gesendet. Nach dem Log In kannst du dein Passwort beliebig ändern.</p>
+            </div>
+        </div>
+        <a href="log_in.php" class="btn btn-light rounded-pill">
+        <i class="fas fa-fw fa-arrow-circle-left"></i> Zurück zum Log In</a>
+    </div>
+
+    </body>
+</html>
